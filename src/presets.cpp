53457e95bd296e6c06f1fac956a06adc46eea204
Circuit Chaos <circuitchaos@email.invalid>
2024-05-27 00:21:56 +0200

    - added preset with only an exchange (in response to NR) - added preset with NR NR

--- src/presets.cpp
+++ src/presets.cpp
@@ -9,6 +9,8 @@ Presets::Presets(const std::string &callsign)
 	presets.push_back(callsign);
 	presets.push_back("PLACEHOLDER"); // TODO hack
 	presets.push_back("PLACEHOLDER"); // TODO hack
+	presets.push_back("PLACEHOLDER"); // TODO hack
+	presets.push_back("NR NR");
 	presets.push_back("AGN");
 	presets.push_back("QRS");
 	presets.push_back("QRL?");
@@ -32,6 +34,11 @@ std::string Presets::getPreset(size_t preset, const std::string &exchange) const
 	if(preset == 3) {
 		/* Hack */
 		return util::format("TU 5NN %s", exchange.c_str());
+	}
+
+	if(preset == 4) {
+		/* Hack */
+		return util::format("%s", exchange.c_str());
 	}
 
 	return presets[preset];
